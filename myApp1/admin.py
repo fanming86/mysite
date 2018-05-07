29ae41d375907895de8c1f9c5b27e58a0e0293cd
fanming86 <969756850@qq.com>
2018-05-07 11:10:26 +0800

    添加分类详细信息

--- myApp1/admin.py
+++ myApp1/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from myApp1.models import Article
 
 
 
@@ -15,7 +14,6 @@ from .models import Post, Category, Tag
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
-admin.site.register(Article)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)

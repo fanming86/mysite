773709d55b83e7c6def660c78e326f59e50dea8d
fanming86 <969756850@qq.com>
2019-02-15 17:03:49 +0800

    .......

--- comment/views.py
+++ comment/views.py
@@ -4,10 +4,10 @@ from django.http import JsonResponse, HttpResponse
 from .models import comForm
 from .models import com
 import time
-import json
+from myApp1.models import Post
 from django.core import serializers
 
-
+from mysite import sendMail
 # Create your views here.
 
 
@@ -15,19 +15,40 @@ def submit_comment(request):
     from1 = comForm(data=request.POST)
     up_load = com()
     if from1.is_valid():
-        up_load.article_id = from1.cleaned_data['article_id']
-        up_load.name = from1.cleaned_data['name']
-        up_load.email = from1.cleaned_data['email']
-        up_load.parent = from1.cleaned_data['parent']
-        up_load.content = from1.cleaned_data['content']
+        article_id = from1.cleaned_data['article_id']
+        up_load.article_id = article_id
+        name = from1.cleaned_data['name']
+        up_load.name = name
+        email = from1.cleaned_data['email']
+        up_load.email = email
+        parent = from1.cleaned_data['parent']
+        up_load.parent = parent
+        content = from1.cleaned_data['content']
+        up_load.content = content
         up_load.save()
-        print(from1.cleaned_data['parent'])
-        return JsonResponse({'msg': 'success!'})
+        msg = 'success!'
+        # 邮件通知18.12.07
+        # 第一部分为通知管理员，有了新的留言
+        # 根据文章id找到文章标题
+        if article_id == 0:
+            title = '留言'
+        else:
+            title = Post.objects.get(pk=article_id).title
+        print(title)
+        sendMail.sendMail(name, email, title, content)
+        # 第二部分，如果留言有了回复，通知被回复的用户
+        # 判断此评论是否是回复
+        if parent:
+            print('####### ', parent.id)
+            comment = com.objects.get(id=parent.id)
+            title = Post.objects.get(pk=comment.article_id).title
+            sendMail.res_mail(comment, title, content)
+
+        return JsonResponse({'msg': msg})
     else:
         print('error')
         return JsonResponse({'msg': 'error'})
 
-    # print(from1)
 
 
 def get_comment(request, i):
